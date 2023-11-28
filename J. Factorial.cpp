4927e9d16c12b7b933e32b28bb9f0331be5b5191
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-28 03:03:54 +0200

    Update J. Factorial.cpp

--- J. Factorial.cpp
+++ J. Factorial.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <string.h>
+
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+ll factorial(int n)
+{
+	if (n == 1)
+	{
+		return 1;
+	}
+
+	return n * factorial(n - 1);
+}
+
+int main()
+{
+	Ragdha
+
+	int n;
+	cin >> n;
+
+	cout << factorial(n);
+    
+	return 0;
+}
 
