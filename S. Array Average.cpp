a3d5247061f8ed3503cb12f4a1070674c018186a
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-28 16:07:55 +0200

    Update S. Array Average.cpp

--- S. Array Average.cpp
+++ S. Array Average.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <string.h>
+#include <cctype> // for tolower function
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+
+double ArrayAverage(double Array[], int n)
+{
+	if (n == 1)
+		return Array[0];
+
+	double sub_result = ArrayAverage(Array, n - 1);
+	sub_result = sub_result * (n - 1);
+
+	return (sub_result + Array[n - 1]) / n;
+}
+
+int main()
+{
+	Ragdha
+
+	int n; double Array[10000];
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> Array[i];
+	}
+
+	cout << fixed << setprecision(6) << ArrayAverage(Array, n);
+
+	return 0;
+}
