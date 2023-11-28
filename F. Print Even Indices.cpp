767f7590db98007b91b85a3320fdd8b2694fcef6
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-28 11:09:23 +0200

    Update F. Print Even Indices.cpp

--- F. Print Even Indices.cpp
+++ F. Print Even Indices.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <string.h>
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+
+void printReversedEvenIndices(ll arr[10000], int n,int index) 
+{
+	if (index >= 0 && index < n) 
+	{
+		if (index % 2 == 0) 
+		{
+			printReversedEvenIndices(arr, n, index + 2);
+			cout << arr[index] << " ";
+		}
+		else
+		{
+			printReversedEvenIndices(arr, n, index + 1);
+		}
+	}
+}
+
+
+int main()
+{
+	Ragdha
+
+	int n;
+	ll arr[10000];
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	printReversedEvenIndices(arr, n, 0);
+
+	return 0;
+}
