1857f8d2e6a14644d324206f1b4b15c4ed7f844b
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-28 03:11:51 +0200

    Update A. Print Recursion.cpp

--- A. Print Recursion.cpp
+++ A. Print Recursion.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <string.h>
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+void Recursion(int n)
+{
+	if (n == 0)
+	{
+		return ;
+	}
+
+	cout << "I love Recursion\n";
+
+	Recursion(n - 1);
+}
+
+int main()
+{
+	Ragdha
+
+	int n;
+	cin >> n;
+
+	Recursion(n); 
+
+	return 0;
+}
