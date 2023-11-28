0ab90b8cb294662f7ffd31e006cd9c67bcbdb665
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-28 10:56:23 +0200

    Update E. Base Converssion.cpp

--- E. Base Converssion.cpp
+++ E. Base Converssion.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <string.h>
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+
+void Recursion( ll n)
+{
+	if (n > 0)
+	{
+		Recursion(n / 2);
+
+		cout << n % 2;
+	}
+}
+
+int main()
+{
+	Ragdha
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		ll n;
+		cin >> n;
+
+		Recursion(n);
+		cout << endl;
+
+	}
+	return 0;
+}
