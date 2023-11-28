bde4655fb250ea89bf3f093a8310aa935030dea7
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-28 15:36:15 +0200

    Update L. Summation.cpp

--- L. Summation.cpp
+++ L. Summation.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <string.h>
+#include <cctype> // for tolower function
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+
+ll ArraySum(ll Array[], ll n)
+{
+	if (n == 1)
+		return Array[0];
+
+	ll sub_result = ArraySum(Array, n - 1);
+	return sub_result + Array[n - 1];
+}
+
+int main()
+{
+	Ragdha
+
+	ll n; ll Array[10000];
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> Array[i];
+	}
+
+	cout << ArraySum(Array, n);
+
+	return 0;
+}
