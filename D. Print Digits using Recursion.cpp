36be51792ed9fd04e2e6770c7edb7c2dd9199e56
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-28 03:53:56 +0200

    Update D. Print Digits using Recursion.cpp

--- D. Print Digits using Recursion.cpp
+++ D. Print Digits using Recursion.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <string.h>
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+
+void Recursion(string n, ll index)
+{
+	if (index == n.size())
+	{
+		return;
+	}
+
+	cout << n[index] << " ";
+	Recursion(n, index + 1);
+}
+
+int main()
+{
+	Ragdha
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		string n;
+		cin >> n;
+
+		Recursion(n,0);
+
+		cout << endl;
+	}
+	return 0;
+}
