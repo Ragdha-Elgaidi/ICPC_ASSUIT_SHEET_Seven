3c2ae711b43c7cd9b56a9904d0f3303e8cca86e2
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-28 03:43:06 +0200

    Update B. Print from 1 to N.cpp

--- B. Print from 1 to N.cpp
+++ B. Print from 1 to N.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <string.h>
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+
+void Recursion(int n,int counter)
+{
+	if (counter > n)
+	{
+		return ;
+	}
+	
+	cout << counter;
+
+	if (counter !=n )
+	{
+		cout << endl;
+	}
+
+	Recursion(n ,counter + 1);
+}
+
+int main()
+{
+	Ragdha
+
+	int n;
+	cin >> n;
+
+	Recursion(n,1);
+    
+	return 0;
+}
