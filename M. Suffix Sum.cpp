bacb644d2dfa3a7d7a4ba4e2634858fefe1fe5df
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-29 06:45:09 +0200

    Update M. Suffix Sum.cpp

--- M. Suffix Sum.cpp
+++ M. Suffix Sum.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <string.h>
+#include <cctype> // for tolower function
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+ll suffix(ll arr[], int len, int cnt)
+{
+	if (cnt == 0)
+	{
+		return 0;
+	}
+
+	return arr[len - 1] + suffix(arr, len - 1, cnt-1);
+}
+
+int main()
+{
+	Ragdha
+
+	ll arr[100000]; int cnt, len;
+	cin >> len >> cnt;
+
+	for (int i = 0; i < len; i++)
+	{
+		cin >> arr[i];
+	}
+    
+	cout << suffix(arr, len, cnt);
+
+	return 0;
+}
