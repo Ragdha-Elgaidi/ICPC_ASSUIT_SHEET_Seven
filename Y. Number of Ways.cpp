89eb8e5542eb6eb5e903b287b64325e5eb8542a4
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-12-01 12:32:46 +0200

    Update Y. Number of Ways.cpp

--- Y. Number of Ways.cpp
+++ Y. Number of Ways.cpp
@@ -13,60 +13,30 @@
 #define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
 
 using namespace std;
-#include <iostream>
-#include <cstring>
 
-#define ll long long
-#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
-
-using namespace std;
-
-// Maximum value for the end point
-const int MAX = 15;
-
-// Memoization array to store previously calculated results
-int memo[MAX + 1];
-
-int countWays(int current, int end)
+int st, ed;
+int get(int pos)
 {
-    // Base case: if the current point is equal to or greater than the end point
-    if (current >= end)
-    {
-        // If the current point is equal to the end point, there is one way to reach it
-        return (current == end) ? 1 : 0;
-    }
+    if (pos > ed)
+        return 0;
 
-    // Check if the result for the current point is already calculated
-    if (memo[current] != -1)
-    {
-        return memo[current];
-    }
+    if (pos == ed)
+        return 1;
 
-    // Recursive case: calculate the number of ways by considering 1, 2, and 3 steps
-    int ways = 0;
-    for (int steps = 1; steps <= 3; ++steps)
-    {
-        ways += countWays(current + steps, end);
-    }
+    return get(pos + 1) + get(pos + 2) + get(pos + 3);
+}
 
-    // Memoize the result for the current point
-    memo[current] = ways;
-
-    return ways;
+void solve() 
+{
+    cin >> st >> ed;
+    cout << get(st);
 }
 
 int main()
 {
     Ragdha
 
-    int start, end;
-    cin >> start >> end;
-
-    // Initialize the memoization array with -1 (indicating not calculated)
-    memset(memo, -1, sizeof(memo));
-
-    // Calculate and print the number of ways
-    cout << countWays(start, end) << endl;
+    solve();
 
     return 0;
 }
