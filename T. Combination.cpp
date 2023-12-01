f8a3e5c8b0950bc4bc8f265a2a8a8d4f950d70cb
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-12-01 10:04:23 +0200

    Update T. Combination.cpp

--- T. Combination.cpp
+++ T. Combination.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <string.h>
+#include <cctype> // for tolower function
 
+#define ll  long long
+#define Ragdha ios_base::sync_with_stdio(0);cin.tie(0);
+
+using namespace std;
+
+
+ll nCr(int n, int r)
+{
+    if (r == 0 || r == n)
+    {
+        return 1;
+    }
+    return (n * nCr(n - 1, r - 1)) / r;
+}
+
+int main()
+{
+    Ragdha
+
+    int N, R;
+    cin >> N >> R;
+
+    cout << nCr(N, R) << endl;
+
+    return 0;
+}
